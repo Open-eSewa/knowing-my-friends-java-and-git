8e3fa48ada66d5eb83170b63d50f8050abefd225
raj242adk <raj242adk@gmail.com>
2023-12-26 13:16:51 +0545

    #2:some spacing changed minor

--- src/np/com/esewa/intern/primitivedatatypes/intexample/ExampleInt.java
+++ src/np/com/esewa/intern/primitivedatatypes/intexample/ExampleInt.java
@@ -1,25 +1,19 @@
 package np.com.esewa.intern.primitivedatatypes.intexample;
 
 public class ExampleInt {
-
     /**
      * It is a signed 32-bit type that has a range
      * from –2,147,483,648 to 2,147,483,647.
      * The default value of int is 0 if not initialized
-     * <p>
      * int myAge;
      * int rollNumber;
      */
-
     static void exampleOfInt() {
         int myAge = 24;
         int myRoll = 119;
-
         /**
          * the below is the example of type casting
          */
-
-
         int intValue = 42;
         byte byteValue = 5;
 
@@ -28,8 +22,6 @@ public class ExampleInt {
 
         // Explicit conversion (narrowing) from int to byte
         byte byteValue2 = (byte) intValue;
-
-
 
         int intValue3 = 56;
 
@@ -47,7 +39,6 @@ public class ExampleInt {
         System.out.println(shortValue);
         System.out.println(byteValue3);
     }
-
     public static void main(String[] args) {
         exampleOfInt();
     }
--- src/np/com/esewa/intern/primitivedatatypes/longExample/ExampleLong.java
+++ src/np/com/esewa/intern/primitivedatatypes/longExample/ExampleLong.java
@@ -1,35 +1,30 @@
 package np.com.esewa.intern.primitivedatatypes.longExample;
 
 public class ExampleLong {
-
     /**
      * long is a signed 64-bit type and is useful for those occasion
      * where an int type is not large enough to hold the desired value
-     *
-     *
      */
     public static void longExample(){
         int lightspeed;
         long days;
         long seconds;
         long distance;
-        // approximate speed of light in miles per second
 
-        /**
-         * so in 1000 days light will travel about 160704000000 miles
-         * which is not possible when we have int
-         *
-         */
-        lightspeed = 186000;
+        lightspeed = 186000; // approximate speed of light in miles per second
         days = 1000; // specify number of days here
         seconds = days * 24 * 60 * 60; // convert to seconds
         distance = lightspeed * seconds; // compute distance
         System.out.print("In " + days);
         System.out.print(" days light will travel about ");
         System.out.println(distance + " miles.");
+        /**
+         * in 1000 days light will travel about 160704000000 miles
+         * which is not possible when we have int
+         *
+         */
 
     }
-
     public static void main(String[] args) {
         longExample();
     }
