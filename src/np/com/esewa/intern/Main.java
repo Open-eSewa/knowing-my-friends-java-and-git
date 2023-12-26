c79d3a392291e3e4093c9c93e34cf77631c74923
milan <milankhanal29@gitlab.com>
2023-12-26 12:24:06 +0545

    implemented logical operator

--- src/np/com/esewa/intern/Main.java
+++ src/np/com/esewa/intern/Main.java
@@ -5,6 +5,22 @@ package np.com.esewa.intern;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hey, Everyone! Welcome again to eSewa");
+                // Logical AND (&&) Operator
+                boolean cond1 = true;
+                boolean cond2 = false;
+                boolean resultAND = cond1 && cond2;
+                System.out.println("Logical AND: " + resultAND);
+        
+                // Logical OR (||) Operator
+                boolean resultOR = cond1 || cond2;
+                System.out.println("Logical OR: " + resultOR);
+        
+                // Logical NOT (!) Operator
+                boolean resultNOT = !cond1;
+                System.out.println("Logical NOT: " + resultNOT);
+        
+                // Combining logical operators
+                boolean complexcond = (cond1 && cond2) || (cond1 && !cond2);
+                System.out.println("Complex cond: " + complexcond);
     }
 }
