8b61e5f35ea5cfc833e78480ed9050db0b613214
WhoIsAoH <ajay.paudel1@gmail.com>
2024-01-12 17:37:59 +0545

    implemented the suggestions

--- src/np/com/esewa/intern/WrapperClass.java
+++ src/np/com/esewa/intern/WrapperClass.java
@@ -1,38 +1,36 @@
 package np.com.esewa.intern;
 
 public class WrapperClass {
-    /**
-     *
-     * @return
-     */
+
     public int wrapperInt() {
         Integer n1 = new Integer(15);
-        Integer n2 = Integer.valueOf("16");     //autoboxing
+        Integer n2 = Integer.valueOf("16");     //autoboxing -> automatic Conversion of primitive type to its corresponding wrapper class object
         int sum = n1.intValue() + n2.intValue();
-//                int sum = n1+n2;      auto-unboxing
+//                int sum = n1+n2;     //auto-unboxing -> automatic conversion of a wrapper class object to its corresponding primitive data type
         return sum;
 //                System.out.println("Sum is :"+ sum);
     }
 
-    public void wrapperChar(){
+    public void wrapperChar() {
         Character c1 = new Character('c');
         char uC = Character.toUpperCase(c1.charValue());
-        System.out.println("Uppercase character: "+uC);
+        System.out.println("Uppercase character: " + uC);
     }
 
-    public void wrapperBool(){
+    public void wrapperBool() {
         Boolean b1 = new Boolean(true);
         Boolean b2 = Boolean.valueOf("false");
         boolean result = b1 || b2;
-        System.out.println("result of boolean or: "+ result);
+        System.out.println("result of boolean or: " + result);
     }
+
     //main class .
     public static void main(String[] args) {
-        WrapperClass main = new WrapperClass();
-        System.out.println("Wrapper int is: "+main.wrapperInt());
-        main.wrapperChar();
-        main.wrapperBool();
-        WrapperDouble wd = new WrapperDouble(4.56);
+        WrapperClass wrapperClass = new WrapperClass();
+        System.out.println("Wrapper int is: " + wrapperClass.wrapperInt());
+        wrapperClass.wrapperChar();
+        wrapperClass.wrapperBool();
+        WrapperDouble wrapperDouble = new WrapperDouble(4.56);
 
     }
 
--- src/np/com/esewa/intern/WrapperDouble.java
+++ src/np/com/esewa/intern/WrapperDouble.java
@@ -2,13 +2,12 @@ package np.com.esewa.intern;
 
 public class WrapperDouble {
     /**
-     *
-     * @param x
+     * @param x as a double to explain the wrapper double class
      */
-    public WrapperDouble(double x){
+    public WrapperDouble(double x) {
         Double d1 = new Double(x);
         Double d2 = Double.valueOf("33.55");
-        double product = d1.doubleValue()*d2.doubleValue();
-        System.out.println("Product of doubles:"+product);
+        double product = d1.doubleValue() * d2.doubleValue();
+        System.out.println("Product of doubles:" + product);
     }
 }
